72f9f77d063de694eccb00983c07bb20f02bebaf
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-01-30 01:18:55 +0530

    Completed round robin scheduling

---
+++ Day-1-LinkedList/src/com/circularlinkedlist/roundrobinschedulingalgo/Process.java
@@ -0,0 +1,15 @@
+package com.circularlinkedlist.roundrobinschedulingalgo;
+
+public class Process {
+    int processID;
+    int burstTime;
+    int priority;
+    Process next;
+
+    public Process(int processID, int burstTime, int priority) {
+        this.processID = processID;
+        this.burstTime = burstTime;
+        this.priority = priority;
+        this.next=null;
+    }
+}
