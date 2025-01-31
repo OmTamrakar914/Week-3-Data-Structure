0977a742ce579ee9333024a2a0ecd535a356ef77
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-01-31 23:40:43 +0530

    Completed Counting sort

---
+++ Day-3-Sorting_Algorithms/src/com/sortingalgorithms/countingsort/StudentAges.java
@@ -0,0 +1,12 @@
+package com.sortingalgorithms.countingsort;
+
+import java.util.Arrays;
+
+public class StudentAges {
+    public static void main(String[] args) {
+        int[] ages = {12, 15, 11, 18, 12, 14, 10, 13, 17, 16, 15, 10};
+
+        int[] sortedAges = CountingSort.countSort(ages);
+        System.out.println(Arrays.toString(sortedAges));
+    }
+}
